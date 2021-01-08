786dbaab3589ae42ff3ef2a37241073f39e6a5b7
NarminGale <narmingale@gmail.com>
2021-01-08 20:58:35 +0400

    Project was completed

---
+++ Counter/counter.js
@@ -0,0 +1,35 @@
+let count = 0
+
+// select the elements
+const value = document.querySelector('.value')
+const decrease = document.querySelector('.decrease')
+const reset = document.querySelector('.reset')
+const increase = document.querySelector('.increase')
+
+// events
+decrease.addEventListener('click', function (e) {
+  count--
+  value.innerText = count
+  checkValue(value)
+})
+reset.addEventListener('click', function (e) {
+  count = 0
+  value.innerText = count
+  checkValue(value)
+})
+increase.addEventListener('click', function (e) {
+  count++
+  value.innerText = count
+  checkValue(value)
+})
+
+// functions
+function checkValue(a) {
+  if (count > 0) {
+    document.querySelector('.value').style.color = 'green'
+  } else if (count < 0) {
+    document.querySelector('.value').style.color = 'red'
+  } else {
+    document.querySelector('.value').style.color = 'black'
+  }
+}
