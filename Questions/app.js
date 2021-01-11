06ed382a001195507e6572075a22c50fe572408b
NarminGale <narmingale@gmail.com>
2021-01-11 19:52:00 +0400

    JS part was completed

---
+++ Questions/app.js
@@ -0,0 +1,30 @@
+// select items
+
+//traversing the dom
+// const btns = document.querySelectorAll('.question-btn')
+// const text = document.querySelectorAll('.question-text')
+
+// btns.forEach(function (btn) {
+//   btn.addEventListener('click', function (e) {
+//     const question = e.currentTarget.parentElement.parentElement
+//     question.classList.toggle('show-text')
+//   })
+// })
+
+//using selectors inside the element
+
+const questions = document.querySelectorAll('.question')
+
+questions.forEach(function (question) {
+  const btn = question.querySelector('.question-btn')
+  btn.addEventListener('click', function () {
+    // for closing other questions when open one question
+    questions.forEach(function (item) {
+      if (item !== question) {
+        item.classList.remove('show-text')
+      }
+    })
+
+    question.classList.toggle('show-text')
+  })
+})
