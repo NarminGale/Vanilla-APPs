b864bda5546da2ccbb887ad8a500e15fa9cb341e
NarminGale <narmingale@gmail.com>
2021-03-31 14:02:06 +0400

    Completed functionality of modal-overlay

--- 06-Modal/app.js
+++ 06-Modal/app.js
@@ -6,7 +6,9 @@ const modalOverlay = document.querySelector('.modal-overlay')
 
 btn.addEventListener('click', function () {
   modalOverlay.style.zIndex = '10'
+  modalOverlay.style.visibility = 'visible'
 })
 closeBtn.addEventListener('click', function () {
   modalOverlay.style.zIndex = '-10'
+  modalOverlay.style.visibility = 'hidden'
 })
